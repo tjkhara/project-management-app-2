204d362c8a915542514e853eef04c90f9ac244d0
Tajeshwar Khara <tkhara@gmail.com>
2020-01-05 21:50:37 +0530

    Code at the end of lecture 14

---
+++ src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.jrp.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepository empRepo;
+
+	@GetMapping("/new")
+	public String displayEmployeeForm(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "new-employee";
+	}
+
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		empRepo.save(employee);
+		return "redirect:/employees/new";
+	}
+}
---
+++ src/main/java/com/jrp/pma/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.jrp.pma.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jrp.pma.entities.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
