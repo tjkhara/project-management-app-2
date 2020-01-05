d84bc347d3f8cc3847c5e6e1c19be3a6da28a619
Tajeshwar Khara <tkhara@gmail.com>
2020-01-05 21:32:25 +0530

    Code at the end of lecture 13

--- src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -1,16 +1,21 @@
 package com.jrp.pma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+	@Autowired
+	ProjectRepository proRepo;
 
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
@@ -18,12 +23,10 @@ public class ProjectController {
 		model.addAttribute("project", project);
 		return "new-project";
 	}
-	
+
 	@PostMapping("/save")
 	public String createProject(Project project, Model model) {
-		
-		
-		
-		return "new-project";
+		proRepo.save(project);
+		return "redirect:/projects/new";
 	}
 }
