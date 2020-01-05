7144ff5390bc274032990de735d332286203d089
Tajeshwar Khara <tkhara@gmail.com>
2020-01-05 20:36:03 +0530

    First commit

---
+++ src/main/java/com/jrp/pma/ProjectManagement2Application.java
@@ -0,0 +1,13 @@
+package com.jrp.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagement2Application.class, args);
+	}
+
+}
