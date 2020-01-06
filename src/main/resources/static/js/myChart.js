811d1f590c7399c9648b50bfef732c80acf1b903
Tajeshwar Khara <tkhara@gmail.com>
2020-01-06 23:51:14 +0530

    Code at the end of lecture 27.

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -19,6 +19,7 @@ new Chart(document.getElementById("myPieChart"), {
         labels: labelData,
         datasets: [{
             label: 'My First dataset',
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
             data: numericData
         }]
     },
